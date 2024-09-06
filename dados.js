6704b2349cd5b76cf111057666077805fc96e6cf
Matheus Silva <69943554+Matheusp-Silva@users.noreply.github.com>
2024-09-06 10:38:50 -0300

    First Commit!
    First Commit!

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e determinação. Bicampeã olímpica, Rebeca acumulou diversas medalhas em competições nacionais e internacionais, elevando o nome do Brasil no cenário da ginástica artística. Sua trajetória inspiradora, marcada por superação de lesões e desafios, a transformou em um ícone do esporte brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginasta ginastica ouro"
+    }, {
+        titulo: "Bia Haddad",
+        descricao: "Bia Haddad é uma tenista brasileira que vem se destacando no cenário internacional. Com um jogo agressivo e consistente, ela conquistou diversos títulos e se tornou uma das principais representantes do tênis feminino brasileiro. Sua força de vontade e determinação a levaram a alcançar grandes feitos na carreira, inspirando novas gerações de tenistas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Haddad_Maia",
+        tags: "tenis tenista"
+    }, {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou o ouro olímpico em Paris 2024. Com técnica apurada e força física, ela se tornou um dos principais nomes do judô brasileiro. Sua dedicação e disciplina a levaram ao topo do pódio, inspirando jovens atletas a seguirem seus sonhos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judoca ouro"
+    }
+];
+
---
+++ script.js
@@ -0,0 +1,45 @@
+function pesquisar() {
+
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase();
+    let titulo = ""
+    let descricao = "";
+    let resultados = "";
+    let tags = "";
+
+    if (!campoPesquisa) {
+        section.innerHTML = "Nenehum atleta foi encontrado na base de dados."
+        return
+    }
+
+    for (let dado of dados) {
+        
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLocaleLowerCase();
+        tags = dado.tags.toLocaleLowerCase();
+
+        if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || dado.tags.includes(campoPesquisa)) {
+            
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Saiba mais</a>
+            </div>
+            `
+        }
+    }
+
+    if (!resultados) {
+        section.innerHTML = "Atleta não registrado na base de dados.";
+        return
+    }
+
+    section.innerHTML = resultados
+}
+
+
+
